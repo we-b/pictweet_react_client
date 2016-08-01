9c6ef0bf6144cb9f15ebb4f3ffeccbc13a8b5eea
NaokiShizuka <naokishizuka14@gmail.com>
2016-08-01 20:55:26 +0900

    VisibleTodoListをVisibleTweetListに変更

--- js/components/App.js
+++ js/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Footer from './Footer'
 import AddTweet from '../containers/AddTweet'
-import VisibleTodoList from '../containers/VisibleTodoList'
+import VisibleTweetList from '../containers/VisibleTweetList'
 
 const App = () => (
   <div>
     <AddTweet />
-    <VisibleTodoList />
+    <VisibleTweetList />
     <Footer />
   </div>
 )
